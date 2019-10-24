e78efd0ab5783b2cfe3b789dd86fd1c44e6de9ec
superazzhole <55227780+superazzhole@users.noreply.github.com>
2019-10-24 14:53:39 +0800

    20191024

---
+++ ui/Assets/Scripts/GameManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;   // 引用 音效 API
+
+public class GameManager : MonoBehaviour
+{
+   public AudioMixer mixer;
+    
+   public void SetVBGM(float value)
+    {
+        mixer.SetFloat("VBGM", value);
+
+    }
+}
