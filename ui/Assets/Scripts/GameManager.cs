d13edceab25311ca66fffcf2ac5f5153bdcc3b6b
superazzhole <55227780+superazzhole@users.noreply.github.com>
2019-10-31 14:50:06 +0800

    20191031

--- ui/Assets/Scripts/GameManager.cs
+++ ui/Assets/Scripts/GameManager.cs
@@ -1,13 +1,29 @@
 ﻿using UnityEngine;
 using UnityEngine.Audio;   // 引用 音效 API
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-   public AudioMixer mixer;
-    
-   public void SetVBGM(float value)
+    public AudioMixer mixer;
+
+    public Text loadingText;
+    public Slider loading;
+
+    public void SetVBGM(float value)
     {
         mixer.SetFloat("VBGM", value);
 
     }
+    public void SetVSFX(float value)
+    { 
+    mixer.SetFloat("VSFX",value);
+        }
+    public void Play()
+    {
+        SceneManager.LoadScene("場景");
+    
+    
+    }
+
 }
