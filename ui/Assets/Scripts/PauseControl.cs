809b77c0d142a6bc62f6e6027e6251db7763759f
supera33hole@gmail.com <supera33hole@gmail.com>
2019-12-05 14:55:02 +0800

    20191205

---
+++ ui/Assets/Scripts/PauseControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseControl : MonoBehaviour
+{
+    [Header("暫停介面")]
+    public Image imagePause;
+    public Sprite spritePause, spritePlay;
+    [Header("暫停")]
+    public bool pause;
+ private void Pause()
+{
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+ 
+            pause = !pause;
+
+            imagePause.sprite = pause ? spritePlay : spritePause;
+            Time.timeScale = pause ? 0 : 1;
+        
+        }
+}
+    private void Update()
+    {
+        Pause();
+    }
+}
+ 
+
+
+  
+	
