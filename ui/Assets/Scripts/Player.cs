dd836daf895b983572bea4a2770f721f771dbc24
superazzhole <55227780+superazzhole@users.noreply.github.com>
2019-12-12 14:53:21 +0800

    20191212

---
+++ ui/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour {
+
+    public int hp = 100;
+    public Slider hpSlider;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        print(other.gameObject);
+
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpSlider.value = hp;
+        }
+
+    }
+    private void OnParticleCollision(GameObject other)
+    {
+        if (other.tag == "陷阱")
+        {
+            int d = other.GetComponent<Trap>().damage;
+            hp -= d;
+            hpSlider.value = hp;
+        }
+
+    }
+}
---
+++ ui/Assets/Scripts/Trap.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class Trap : MonoBehaviour
+{
+    [Header("傷害值"), Range(1, 50)]
+    public int damage = 10;
+
+
+		
+	}
+
