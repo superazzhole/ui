1377b778a9cec4a8de84fc2c47107da730fd5e17
superazzhole <55227780+superazzhole@users.noreply.github.com>
2019-12-19 15:41:12 +0800

    20191219

--- ui/Assets/Scripts/Player.cs
+++ ui/Assets/Scripts/Player.cs
@@ -7,6 +7,13 @@ public class Player : MonoBehaviour {
     public int hp = 100;
     public Slider hpSlider;
 
+    public Text textChicken;
+    public int chickenCount;
+    public int chickenTotal;
+
+    public Text textTime;
+    public float gameTime;
+
     private void OnTriggerEnter(Collider other)
     {
         print(other.gameObject);
@@ -17,7 +24,19 @@ public class Player : MonoBehaviour {
             hp -= d;
             hpSlider.value = hp;
         }
+        if (other.tag == "雞腿")
+        {
+            chickenCount++;
+            textChicken.text = "Chicken : " + chickenCount + " / " + chickenTotal;
+            Destroy(other.gameObject);
 
+        }
+
+        if(other.name == "終點" && chickenCount == chickenTotal)
+        {
+            print("過關");
+        }
+ 
     }
     private void OnParticleCollision(GameObject other)
     {
@@ -28,5 +47,22 @@ public class Player : MonoBehaviour {
             hpSlider.value = hp;
         }
 
+    }
+    private void Start()
+    {
+        chickenTotal = GameObject.FindGameObjectsWithTag("雞腿").Length;
+        textChicken.text = "Chicken : 0/" + chickenTotal;
+
+    }
+
+    private void Update()
+    {
+        UpdateTime();
+    }
+
+    private void UpdateTime()
+    {
+        gameTime += Time.deltaTime;
+        textTime.text = gameTime.ToString("F2");
     }
 }
